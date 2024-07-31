c64cbe54aee245f97a768ec0ee1ceea54db1b012
tonymk0987 <kamandetonymuriuki@gmail.com>
2024-07-31 03:50:11 +0300

    Modified

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,35 +17,52 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
-
-    def post(self):
-
+   def get(self):
+       
+       response_dict_list = [n.to_dict() for n in Plant.query.all()]
+       response=make_response(
+           response_dict_list,
+           200
+       )
+       return response
+   
+   def post(self):
         data = request.get_json()
+        new_record = Plant(
+        name=data['name'],
+        image=data['image'],
+        price=data['price']
+    )
+        
+        db.session.add(new_record)
+        db.session.commit()
+        
+        response_dict = new_record.to_dict()
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+        response = make_response(
+            response_dict,
+            201,
         )
 
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-api.add_resource(Plants, '/plants')
+        return response
+   
+api.add_resource(Plants ,'/plants')
+       
 
 class PlantByID(Resource):
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-api.add_resource(PlantByID, '/plants/<int:id>')
+    def get(self,id):
         
+        plant_dict = Plant.query.filter_by(id=id).first().to_dict()
+        
+        response = make_response(
+            plant_dict,
+            200,
+        )
 
+        return response
+
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+    
 if __name__ == '__main__':
-    app.run (port=5555, debug=True)
+    app.run(port=5555, debug=True)
